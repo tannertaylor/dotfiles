796dba9042df5e303438ecccb961e3ec73a266ce
Tanner Taylor <tanneretaylor@outlook.com>
2021-11-11 20:10:31 -0700

    Added keymap to allow ctrl+backspace to delete the previous word.

--- neovim/.config/nvim/lua/keymap.lua
+++ neovim/.config/nvim/lua/keymap.lua
@@ -1,5 +1,9 @@
 local map_key = vim.api.nvim_set_keymap
 local options = { noremap = true }
 
+-- map normal-mode keys
 map_key("n", "<C-w><Bslash>", ":vsp<CR>", options)
 map_key("n", "<C-w>-", ":sp<CR>", options)
+
+-- map insert-mode keys
+map_key("i", "", "<C-w>", options) -- convert ctrl+backspace to delete previous word
