297abc7c21a39ab5ea6629eedde689c8ce1d93ea
Tanner Taylor <tanner@ttpro.dev>
2021-11-12 20:21:49 -0700

    Fixed hotkey popup fonts.

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -6,6 +6,11 @@ local theme = dofile(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- theme overrides
 theme.useless_gap = 8
-theme.font = "agave Nerd Font 4"
+
+-- set fonts
+local font = "agave Nerd Font 4"
+theme.font = font
+theme.hotkeys_font = font
+theme.hotkeys_description_font = font
 
 return theme
