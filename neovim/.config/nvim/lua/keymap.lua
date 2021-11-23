19600608108d78692a0269072741099599e12c42
Tanner Taylor <tanner@ttpro.dev>
2021-11-23 14:40:48 -0700

    A couple keymaps and temporarily disabling the colorscheme.

--- neovim/.config/nvim/lua/keymap.lua
+++ neovim/.config/nvim/lua/keymap.lua
@@ -1,9 +1,14 @@
 local map_key = vim.api.nvim_set_keymap
 local options = { noremap = true }
 
+-- map leader key
+vim.g.mapleader = " "
+
 -- map normal-mode keys
 map_key("n", "<C-w><Bslash>", ":vsp<CR>", options)
 map_key("n", "<C-w>-", ":sp<CR>", options)
+map_key("n", "<C-kPoint>", ":Telescope lsp_code_actions", options)
+map_key("n", "gd", ":lua vim.lsp.buf.definition()<CR>", options)
 
 -- map insert-mode keys
 map_key("i", "", "<C-w>", options) -- convert ctrl+backspace to delete previous word
