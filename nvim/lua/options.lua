c2847a490b1f03ce4d4be1544838d27ecc0f9c21
Tanner Taylor <tanner@ttpro.dev>
2021-12-24 23:53:58 -0700

    Removed auto_commands.lua, installed new colorschemes, and set a bunch of new options.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,19 +1,35 @@
-local options = vim.opt
-local globals = vim.g
-
--- tab set-up
 local tab_width = 4
-options.tabstop = tab_width
-options.softtabstop = tab_width
-options.shiftwidth = tab_width
-options.expandtab = true
 
--- line numbers
-options.number = true
-options.relativenumber = true
+local options = {
+    backup = false,
+    cursorline = true,
+    expandtab = true,
+    fileencoding = "utf-8",
+    ignorecase = true,
+    mouse = "a",
+    number = true,
+    relativenumber = true,
+    shiftwidth = tab_width,
+    showmode = false,
+    showtabline = tab_width,
+    signcolumn = "yes",
+    smartcase = true,
+    smartindent = true,
+    softtabstop = tab_width,
+    splitbelow = true,
+    splitright = true,
+    swapfile = false,
+    tabstop = tab_width,
+    undofile = true,
+    updatetime = 750,
+    writebackup = false
+}
 
--- set theme style to dark
-globals.vscode_style = "dark"
+for key, value in pairs(options) do
+    vim.opt[key] = value
+end
+
+local globals = vim.g
 
 -- nerdtree settings
 globals.NERDTreeShowHidden = true
