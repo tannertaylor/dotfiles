e2f7b9629c01e577bb40d6b3e1026116656b670e
Tanner Taylor <me@tannert.dev>
2021-12-25 21:51:09 -0700

    Updated to make safe calls to plugin libraries, in case they haven't been installed yet.

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,4 +1,7 @@
-local toggleterm = require("toggleterm")
+local success, toggleterm = pcall(require, "toggleterm")
+if not success then
+    return
+end
 
 toggleterm.setup({
     direction = "float",
