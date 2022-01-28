0e2e5fb3c0f805d76b98f85d0748a1fedaf1a486
Tanner Taylor <tanner@ttpro.dev>
2022-01-28 13:05:08 -0700

    Updated to remove oh-my-zsh and install powerlevel10k

--- stow.sh
+++ stow.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-stow --ignore=zsh -t $HOME/.config .
-stow -t $HOME zsh
+
+ZSHENV_FILE=/etc/zsh/zshenv
+if ! [ -f $ZSHENV_FILE ] || ! grep "export ZDOTDIR=" $ZSHENV_FILE > /dev/null 2>&1; then
+    echo "export ZDOTDIR=\$HOME/.config/zsh" | sudo tee -a /etc/zsh/zshenv > /dev/null 2>&1
+fi
+
+stow -t $HOME/.config .
