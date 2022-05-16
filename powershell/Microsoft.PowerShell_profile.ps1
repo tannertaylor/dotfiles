efd786679a81e9cb3bf520ea71a4c8c72fdc1163
Tanner Taylor <tanner@ttpro.dev>
2022-05-16 10:33:40 -0700

    Added ll alias to powershell.

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,7 @@
+# Oh-My-Posh
 oh-my-posh init pwsh --config ~/.poshthemes/night-owl.omp.json | Invoke-Expression
 
+# Aliases
 Set-Alias -Name cl -Value Clear-Host 
+Set-Alias -Name ll -Value Get-ChildItem
 Set-Alias -Name tf -Value terraform
