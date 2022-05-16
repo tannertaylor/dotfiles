d7b2298df5517ddd734337a1441d137597a09822
Tanner Taylor <me@tannert.dev>
2022-05-16 10:35:43 -0700

    Set go path to code/go

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -5,3 +5,6 @@ oh-my-posh init pwsh --config ~/.poshthemes/night-owl.omp.json | Invoke-Expressi
 Set-Alias -Name cl -Value Clear-Host 
 Set-Alias -Name ll -Value Get-ChildItem
 Set-Alias -Name tf -Value terraform
+
+# Environment variables
+$env:GOPATH = "$env:HOME/code/go"
