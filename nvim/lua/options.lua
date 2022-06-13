ec67465ec1f3a46643877b1f7db999579832127f
Tanner Taylor <me@tannert.dev>
2022-06-12 23:03:29 -0700

    Minor cleanup in options.lua.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,6 +1,8 @@
+-- aliases
 local set_buffer_option = vim.api.nvim_buf_set_option
 local set_window_option = vim.api.nvim_win_set_option
 
+-- buffer options
 local set_buffer_options = function(args)
   local bufnr = args.buf
 
@@ -11,10 +13,12 @@ local set_buffer_options = function(args)
   set_buffer_option(bufnr, 'expandtab', true)
 end
 
+vim.api.nvim_create_autocmd('BufEnter', {
+  callback = set_buffer_options
+})
+
+-- window options
 -- line numbers
 set_window_option(0, 'number', true)
 set_window_option(0, 'relativenumber', true)
 
-vim.api.nvim_create_autocmd('BufEnter', {
-  callback = set_buffer_options
-})
