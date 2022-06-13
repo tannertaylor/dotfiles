ac7514b3f37b13351da03d5daaf142b11522f9ee
Tanner Taylor <tanner@ttpro.dev>
2022-06-12 23:01:16 -0700

    Fixed buffer options with autocmd and started lsp stuff.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,22 +1,20 @@
--- functions
-local function set_global_option(option, value)
-  vim.api.nvim_set_option(option, value)
-end
+local set_buffer_option = vim.api.nvim_buf_set_option
+local set_window_option = vim.api.nvim_win_set_option
 
-local function set_window_option(option, value)
-  vim.api.nvim_win_set_option(0, option, value)
-end
+local set_buffer_options = function(args)
+  local bufnr = args.buf
 
-local function set_buffer_option(option, value)
-  vim.api.nvim_buf_set_option(0, option, value)
+  -- tabs
+  local tab_width = 2
+  set_buffer_option(bufnr, 'tabstop', tab_width)
+  set_buffer_option(bufnr, 'shiftwidth', tab_width)
+  set_buffer_option(bufnr, 'expandtab', true)
 end
-
--- tabs
-local tab_width = 2
-set_buffer_option('tabstop', tab_width)
-set_buffer_option('shiftwidth', tab_width)
-set_buffer_option('expandtab', true)
 
 -- line numbers
-set_window_option('number', true)
-set_window_option('relativenumber', true)
+set_window_option(0, 'number', true)
+set_window_option(0, 'relativenumber', true)
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  callback = set_buffer_options
+})
