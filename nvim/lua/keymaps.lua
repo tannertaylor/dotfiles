c75bcb1a0a57f3f7c53552fab7a5d82159593a21
Tanner Taylor <me@tannert.dev>
2022-06-14 12:55:40 -0700

    Created a basic lua lsp setup.

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,21 +1,29 @@
--- functions
-local function set_normal_keymap(keymap, target)
-  vim.api.nvim_set_keymap('n', keymap, target, { noremap = true })
+-- define keymaps per mode
+local normal_keymaps = {
+  -- general keymaps
+  ['<c-s>'] = '<cmd>w<CR>',
+  ['<c-z>'] = 'u',
+
+  -- window keymaps
+  ['<c-h>'] = '<c-w>h',
+  ['<c-j>'] = '<c-w>j',
+  ['<c-k>'] = '<c-w>k',
+  ['<c-l>'] = '<c-w>l'
+}
+
+local insert_keymaps = {
+  -- general keymaps
+  ['<c-s>'] = '<cmd>w<CR>',
+  ['<c-z>'] = '<Esc>ua'
+}
+
+-- apply keymaps
+local options = { noremap = true }
+
+for keymap, target in pairs(normal_keymaps) do
+  vim.api.nvim_set_keymap('n', keymap, target, options)
 end
 
-local function set_insert_keymap(keymap, target)
-  vim.api.nvim_set_keymap('i', keymap, target, { noremap = true })
+for keymap, target in pairs(insert_keymaps) do
+  vim.api.nvim_set_keymap('i', keymap, target, options)
 end
-
--- keymaps
--- general keymaps
-set_normal_keymap('<c-s>', ':w<CR>')
-set_insert_keymap('<c-s>', '<Esc>:w<CR>a')
-set_normal_keymap('<c-z>', 'u')
-set_insert_keymap('<c-z>', '<Esc>ua')
-
--- pane keymaps
-set_normal_keymap('<c-j>', '<c-w>j')
-set_normal_keymap('<c-k>', '<c-w>k')
-set_normal_keymap('<c-h>', '<c-w>h')
-set_normal_keymap('<c-l>', '<c-w>l')
