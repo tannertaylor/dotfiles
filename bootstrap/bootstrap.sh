f55bb94cd79130bfab94817423317d704f867cad
Tanner Taylor <me@tannert.dev>
2022-06-17 17:27:30 -0700

    Added yay and omnisharp to bootstrap.sh

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -2,7 +2,13 @@
 
 # install required packages
 # TODO: handle different distros
-sudo pacman -S neovim zsh lua-language-server
+sudo pacman -S base-devel neovim zsh lua-language-server
+
+mkdir $HOME/.cache
+git clone https://aur.archlinux.org/yay.git $HOME/.cache/yay
+makepkg -si $HOME/.cache/yay
+
+yay -S --answerdiff None --answerclean All omnisharp-roslyn-bin
 
 # install the 'packer' neovim package and install/sync all neovim plugins.
 PACKAGE_PATH=$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
