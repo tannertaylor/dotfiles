0ab460b7d3773aa20aae829c09a16eefaa316f32
Tanner Taylor <tanner@ttpro.dev>
2022-06-18 15:36:53 -0700

    Added keymap for NvimTreeToggle and fixed issue with matching open-close pairs.

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -10,13 +10,16 @@ local map = vim.keymap.set
 map({ 'n', 'i' }, '<c-s>', '<cmd>w<cr>')
 map('n', '<c-z>', 'u')
 map('i', '<c-z>', '<esc>ua')
-map('n', '<c-_>', '<cmd>CommentToggle<cr>') -- the underscore _ maps to forward slash
+map('n', '<c-_>', '<cmd>CommentToggle<cr>') -- the underscore maps to forward slash
 
 -- window keymaps
 map('n', '<c-h>', '<c-w>h')
 map('n', '<c-j>', '<c-w>j')
 map('n', '<c-k>', '<c-w>k')
 map('n', '<c-l>', '<c-w>l')
+
+-- keymaps to plugins
+map('n', '<c-n>', '<cmd>NvimTreeToggle<cr>')
 
 -- map auto-closing character pairs
 local char_pairs = {
@@ -28,15 +31,26 @@ local char_pairs = {
 }
 
 for open_char, close_char in pairs(char_pairs) do
-  map('i', open_char, open_char .. close_char .. '<left>', {})
-  map('i', close_char, function()
-    local next_char = cursor.get_char()
-    if (next_char and next_char == close_char) then
-      return '<right>'
-    end
+  if (open_char == close_char) then
+    map('i', open_char, function()
+      local next_char = cursor.get_char()
+      if (next_char and next_char == close_char) then
+        return '<right>'
+      end
 
-    return close_char
-  end, { expr = true })
+      return open_char .. close_char .. '<left>'
+    end, { expr = true })
+  else
+    map('i', open_char, open_char .. close_char .. '<left>', {})
+    map('i', close_char, function()
+      local next_char = cursor.get_char()
+      if (next_char and next_char == close_char) then
+        return '<right>'
+      end
+
+      return close_char
+    end, { expr = true })
+  end
 end
 
 map('i', '<tab>', function()
