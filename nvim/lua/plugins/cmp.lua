e724a47a8d9524f11830e4159f7aef4138d33905
Tanner Taylor <me@tannert.dev>
2022-06-19 18:11:58 -0700

    Added auto-completion.

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,16 @@
+local cmp = require('cmp')
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end
+  },
+  mapping = cmp.mapping.preset.insert({
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' }
+  })
+})
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -58,4 +58,17 @@ packer.startup(function(use)
       })
     end
   })
+
+  use('L3MON4D3/LuaSnip')
+  use('saadparwaiz1/cmp_luasnip')
+
+  use('hrsh7th/cmp-nvim-lsp')
+  use('hrsh7th/cmp-buffer')
+  use('hrsh7th/cmp-path')
+  use({
+    'hrsh7th/nvim-cmp',
+    config = function()
+      require('plugins.cmp')
+    end
+  })
 end)
