e4e4d589f709b25ece10a8d80aca9afedcdeaf94
Tanner Taylor <tanneretaylor95@gmail.com>
2024-03-12 19:30:56 -0700

    Added starship, lazy.nvim, and tmux

---
+++ config/nvim/init.lua
@@ -0,0 +1,17 @@
+require("options")
+
+local lazyPath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazyPath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazyPath,
+  })
+end
+vim.opt.rtp:prepend(lazyPath) -- add to runtimepath (rtp)
+
+require("lazy").setup("plugins")
+
---
+++ config/nvim/lua/options.lua
@@ -0,0 +1,10 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.statuscolumn = " %3.3l %3.3r  "
+
+local tabSize = 4
+vim.o.tabstop = tabSize
+vim.o.softtabstop = tabSize
+vim.o.shiftwidth = tabSize
+vim.o.expandtab = true
+
