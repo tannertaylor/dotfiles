92e7ff5f12740aa06aae9a3dec0914ffa265742d
Tanner Taylor <tanneretaylor95@gmail.com>
2024-05-17 10:15:14 -0700

    lazy-lock.json and updated arch_packages.sh

--- init/arch_packages.sh
+++ init/arch_packages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo pacman --noconfirm -S `
-    base-devel `
-    zsh starship neofetch `
-    git lazygit `
-    neovim tree-sitter `
-    docker `
-    ripgrep
+    base-devel ` # AUR packages
+    zsh starship neofetch ` # Shells and shell-related
+    git lazygit ` # VCS
+    neovim tree-sitter ` # Neovim
+    docker docker-compose ` # Containerization
+    ripgrep stow # Utilities
