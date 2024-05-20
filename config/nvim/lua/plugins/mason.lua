e28c3c8c122c9cd0b4b2c54a9b2c7a22b97ceb2d
Tanner Taylor <tanneretaylor95@gmail.com>
2024-05-20 15:28:30 -0700

    Moved mason tool install to mason-tool-install plugin, which has better commands for headless mode

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -2,31 +2,46 @@
 
 ---@type LazySpec
 return {
-  -- use mason-lspconfig to configure LSP installations
   {
-    "williamboman/mason-lspconfig.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+    "WhoIsSethDaniel/mason-tool-installer",
+    opts = {
+      ensure_installed = {
         "lua_ls",
         "rust_analyzer",
         "jsonls",
         "dockerls",
         "yamlls",
-      })
-    end,
-  },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  {
-    "jay-babu/mason-null-ls.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "prettier",
         "stylua",
-      })
-    end,
+      },
+    },
   },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    opts = function(_, opts) opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {}) end,
-  },
+
+  -- use mason-lspconfig to configure LSP installations
+  -- {
+  --   "williamboman/mason-lspconfig.nvim",
+  --   opts = function(_, opts)
+  --     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+  --       "lua_ls",
+  --       "rust_analyzer",
+  --       "jsonls",
+  --       "dockerls",
+  --       "yamlls",
+  --     })
+  --   end,
+  -- },
+  -- -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  -- {
+  --   "jay-babu/mason-null-ls.nvim",
+  --   opts = function(_, opts)
+  --     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+  --       "prettier",
+  --       "stylua",
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "jay-babu/mason-nvim-dap.nvim",
+  --   opts = function(_, opts) opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {}) end,
+  -- },
 }
