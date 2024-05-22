6cafc3f6535382476ddf8db9e39b2fbaa9fba81c
Tanner Taylor <tanneretaylor95@gmail.com>
2024-05-22 10:34:45 -0700

    Added line wrapping options

--- config/nvim/lua/polish.lua
+++ config/nvim/lua/polish.lua
@@ -1,1 +1,10 @@
 vim.opt.shell = "/usr/bin/zsh"
+
+-- Line wrapping
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.breakindentopt = {
+  shift = 2,
+  sbr = true,
+}
+vim.opt.showbreak = ">>"
