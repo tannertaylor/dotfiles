0c3ef9b0405dfde424f4645a24d993c7c3d12c8a
Tanner Taylor <tanneretaylor95@gmail.com>
2024-05-25 17:10:58 -0700

    Added go language server

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -13,35 +13,8 @@ return {
         "yamlls",
         "prettier",
         "stylua",
+        "gopls",
       },
     },
   },
-
-  -- use mason-lspconfig to configure LSP installations
-  -- {
-  --   "williamboman/mason-lspconfig.nvim",
-  --   opts = function(_, opts)
-  --     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-  --       "lua_ls",
-  --       "rust_analyzer",
-  --       "jsonls",
-  --       "dockerls",
-  --       "yamlls",
-  --     })
-  --   end,
-  -- },
-  -- -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  -- {
-  --   "jay-babu/mason-null-ls.nvim",
-  --   opts = function(_, opts)
-  --     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-  --       "prettier",
-  --       "stylua",
-  --     })
-  --   end,
-  -- },
-  -- {
-  --   "jay-babu/mason-nvim-dap.nvim",
-  --   opts = function(_, opts) opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {}) end,
-  -- },
 }
