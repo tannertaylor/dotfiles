77482748ac27bd537aa9c28b7159b41cf5978f02
Tanner Taylor <tanneretaylor95@gmail.com>
2024-06-21 17:06:05 -0700

    Added mappings for telescope prompts

--- user/config/neovim/plugins/telescope.nix
+++ user/config/neovim/plugins/telescope.nix
@@ -6,5 +6,12 @@
       enable = true;
       settings.hijack_netrw = true;
     };
+
+    settings.defaults = {
+      mappings.i = {
+        "<C-j>" = { __raw = "require('telescope.actions').move_selection_next"; };
+	"<C-k>" = { __raw = "require('telescope.actions').move_selection_previous"; };
+      };
+    };
   };
 }
