35724eb0db9dbe569f54bdfb8d523950a6d4be1b
Tanner Taylor <tanner@tannertaylor.dev>
2024-06-22 17:14:16 -0700

    Removed init dir

--- init/arch_packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo pacman --noconfirm -S `
-    base-devel ` # AUR packages
-    zsh starship neofetch ` # Shells and shell-related
-    git lazygit ` # VCS
-    neovim tree-sitter ` # Neovim
-    docker docker-compose ` # Containerization
-    ripgrep stow # Utilities
