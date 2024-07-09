c3a219c060a984546939635ba903e5fd699ae10c
Tanner Taylor <tanner@tannertaylor.dev>
2024-07-08 18:24:00 -0700

    Set gtk2 config location to resolve problems with overwriting

--- user/config/gtk/gtk.nix
+++ user/config/gtk/gtk.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   gtk = {
     enable = true;
     theme = {
       package = pkgs.gruvbox-gtk-theme;
       name = "Gruvbox-Dark-BL-LB";
     };
+    gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
   };
 }
