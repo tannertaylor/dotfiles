f774ea6d811b0dbe716eeac6ec3d5652f452d91a
Tanner Taylor <tanner@tannertaylor.dev>
2024-07-14 19:36:24 -0700

    Added function to run nixos config and home-manager config

--- user/config/bash/bash.nix
+++ user/config/bash/bash.nix
@@ -14,6 +14,12 @@
       eval "$(starship init bash)"
       eval "$(zoxide init bash)"
       stty werase '^h' # Set ctrl+backspace (^h) to erase previous word in TTY settings.
+
+      REPOS="$HOME/code"
+      NIX_FLAKE_PATH="$REPOS/tannertaylor/dotfiles"
+
+      function sys-rebuild() { sudo nixos-rebuild switch --flake "$NIX_FLAKE_PATH"; }
+      function hm-rebuild() { home-manager switch --flake "$NIX_FLAKE_PATH" -b bak; }
     '';
   };
 }
