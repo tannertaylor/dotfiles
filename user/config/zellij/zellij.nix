349b431bc008589ff085cf36feed73cbe1ce75ae
Tanner Taylor <tanner@tannertaylor.dev>
2024-07-15 20:01:27 -0700

    Creating zellij config manually and added keybinds
    Had to create the config file manually because the built-in KDL
    serializer used by home-manager doesn't currently allow for multiple
    entries with the same "name", i.e., the word of an instruction.

--- user/config/zellij/zellij.nix
+++ user/config/zellij/zellij.nix
@@ -2,9 +2,19 @@
   programs.zellij = {
     enable = true;
     enableBashIntegration = true;
-    settings = {
-      theme = "gruvbox-dark";
-      hide_session_name = true;
-    };
   };
+
+  xdg.configFile."zellij/config.kdl".text = ''
+    theme "gruvbox-dark"
+    hide_session_name true
+
+    keybinds {
+      unbind "Ctrl h" "Ctrl n"
+
+      shared { 
+        bind "Ctrl d" { SwitchToMode "move"; }
+        bind "Ctrl r" { SwitchToMode "resize"; }
+      }
+    }
+  '';
 }
