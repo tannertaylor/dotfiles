c1c23af4f515ce94e7287ed677f2aff4e058ecf2
Tanner Taylor <tanner@tannertaylor.dev>
2024-07-16 16:31:08 -0700

    Update custom layout and add swap layouts

--- user/config/zellij/zellij.nix
+++ user/config/zellij/zellij.nix
@@ -14,7 +14,7 @@
     }
 
     keybinds {
-      unbind "Ctrl n"
+      unbind "Ctrl n" "Ctrl h"
 
       shared { 
         bind "Ctrl d" { SwitchToMode "move"; }
@@ -23,13 +23,6 @@
 
       shared_except "locked" "normal" {
         bind "Ctrl c" { SwitchToMode "normal"; }
-      }
-
-      normal {
-        bind "Ctrl h" { MoveFocus "Left"; }
-        bind "Ctrl j" { MoveFocus "Down"; }
-        bind "Ctrl k" { MoveFocus "Up"; }
-        bind "Ctrl l" { MoveFocus "Right"; }
       }
 
       tab {
@@ -41,14 +34,56 @@
 
   xdg.configFile."zellij/layouts/default.kdl".text = ''
     layout {
-      pane size=1 borderless=true {
-        plugin location="zellij:tab-bar"
+      default_tab_template {
+        pane size=1 borderless=true {
+          plugin location="zellij:tab-bar"
+        }
+
+        children
+
+        pane size=1 borderless=true {
+          plugin location="zellij:status-bar"
+        }
       }
 
-      pane
+      swap_tiled_layout name="basic" {
+        tab split_direction="vertical" {
+          pane size="65%"
+          pane split_direction="horizontal" { children; }
+        }
+      }
 
-      pane size=1 borderless=true {
-        plugin location="zellij:status-bar"
+      swap_tiled_layout name="dev" {
+        tab split_direction="vertical" {
+          pane size="75%" {
+            pane size="75%"
+            pane stacked=true { children; }
+          }
+          pane
+        }
+      }
+
+      swap_floating_layout name="basic" {
+        floating_panes max_panes=1 {
+          pane { x "5%"; y "5%"; width "90%"; height "90%"; }
+        }
+
+        floating_panes max_panes=10 {
+          pane { x "5%"; y 1; width "90%"; height "90%"; }
+          pane { x "5%"; y 2; width "90%"; height "90%"; }
+          pane { x "5%"; y 3; width "90%"; height "90%"; }
+          pane { x "5%"; y 4; width "90%"; height "90%"; }
+          pane { x "5%"; y 5; width "90%"; height "90%"; }
+          pane { x "5%"; y 6; width "90%"; height "90%"; }
+          pane { x "5%"; y 7; width "90%"; height "90%"; }
+          pane { x "5%"; y 8; width "90%"; height "90%"; }
+          pane { x "5%"; y 9; width "90%"; height "90%"; }
+          pane focus=true { x "5%"; y 10; width "90%"; height "90%"; }
+        }
+      }
+
+      swap_floating_layout name="staggered" {
+        floating_panes
       }
     }
   '';
