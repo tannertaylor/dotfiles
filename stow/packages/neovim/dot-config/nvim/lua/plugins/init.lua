a9b9fd4ae1fbff41c034659fbdc930e3ec924460
Tanner Taylor <tanner@tannertaylor.dev>
2024-10-06 12:35:44 -0700

    Update to clone lazy.nvim if it is missing

--- stow/packages/neovim/dot-config/nvim/lua/plugins/init.lua
+++ stow/packages/neovim/dot-config/nvim/lua/plugins/init.lua
@@ -1,4 +1,10 @@
-local lazy_path = "/home/tanner/.local/share/lazy.nvim"
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim" --"/home/tanner/.local/share/lazy.nvim"
+
+if not vim.loop.fs_stat(lazy_path) then
+  local lazy_repo = "https://github.com/folke/lazy.nvim"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazy_repo, lazy_path })
+end
+
 vim.opt.rtp:prepend(lazy_path)
 
 local plugins = {
