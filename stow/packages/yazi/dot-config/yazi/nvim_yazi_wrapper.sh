6afa3de8fa910e09ec649c815bf026be869e5fb5
Tanner Taylor <tanner@tannertaylor.dev>
2024-10-06 16:19:00 -0700

    Fix yazi nvim opener

--- stow/packages/yazi/dot-config/yazi/nvim_yazi_wrapper.sh
+++ stow/packages/yazi/dot-config/yazi/nvim_yazi_wrapper.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env
+#!/usr/bin/env bash
 
-if [ -z "$NVIM_REMOTE" ]; then
+if [[ -z $NVIM_REMOTE ]]; then
   nvim "$@"
 else
   NVIM_FILES=$(echo "$@" | tr ' ' '~')
-  nvim --server "$NVIM_REMOTE" --remote-send "<c-\\><c-n>:close! | :lua tt.utils.edit_multiple_files('$NVIM_FILES')<cr>"
+  echo "@ = $@"
+  echo "NVIM_FILES = $NVIM_FILES"
+  nvim --server "$NVIM_REMOTE" --remote-send "<c-\\><c-n>:close! | :lua tt.utils.edit_multiple_files(\"$NVIM_FILES\")<cr>"
 fi
 
