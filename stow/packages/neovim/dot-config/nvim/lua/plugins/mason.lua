6fd4771f29a54525b0927224034649501cd28842
Tanner Taylor <tanner@tannertaylor.dev>
2024-12-06 09:28:59 -0700

    Update to use mason for language servers

---
+++ stow/packages/neovim/dot-config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "awk_ls",
+        "bashls",
+        "bufls", -- protobuf
+        "docker_compose_language_service",
+        "dockerls",
+        "gopls",
+        "graphql",
+        "jsonls",
+        "lua_ls",
+        "marksman", -- markdown
+        "powershell_es",
+        "rust_analyzer",
+        "sqlls",
+        "terraformls",
+        "yamlls",
+      },
+      handlers = {
+        function (server_name)
+          require("lspconfig")[server_name].setup({ })
+        end,
+      },
+    },
+  },
+  "neovim/nvim-lspconfig",
+}
