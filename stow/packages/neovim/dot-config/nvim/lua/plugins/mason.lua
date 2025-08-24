df5e0bab8059d44b25f87f8fffc30722b89faf0a
Tanner Taylor <tanner@tannertaylor.dev>
2025-08-23 18:04:30 -0700

    Remove unused language servers

--- stow/packages/neovim/dot-config/nvim/lua/plugins/mason.lua
+++ stow/packages/neovim/dot-config/nvim/lua/plugins/mason.lua
@@ -9,16 +9,12 @@ return {
       ensure_installed = {
         "awk_ls",
         "bashls",
-        "bufls", -- protobuf
         "docker_compose_language_service",
         "dockerls",
-        "gopls",
         "graphql",
         "jsonls",
         "lua_ls",
         "marksman", -- markdown
-        "nil_ls", -- nix
-        "powershell_es",
         "rust_analyzer",
         "sqlls",
         "terraformls",
