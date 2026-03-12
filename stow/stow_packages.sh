7a9c7856db198291da4076ad45b411d7ab3ad819
Tanner Taylor <tanner@tannertaylor.dev>
2026-03-12 09:18:15 -0700

    Add starship config

--- stow/stow_packages.sh
+++ stow/stow_packages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-stow -S bash common desktops ghostty git hyprland neovim systemd waybar yazi zsh
+stow -S bash common desktops ghostty git hyprland neovim starship systemd waybar yazi zsh
 
